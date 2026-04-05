92ab5150d6542a8f9d049974b17944f07feaf489
malika19-cmd <wadhwamalika50@gmail.com>
2026-04-05 11:19:36 +0530

    server.js

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const express = require("express"); const http = require("http"); const { Server
 const app = express(); const server = http.createServer(app); const io = new Server(server);
 app.use(express.static("."));
 
+// Route for homepage
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// IMPORTANT for Render
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(Server running on port ${PORT});
+});
+
 let settings = {
   pomodoro: 25,
   shortBreak: 5,
