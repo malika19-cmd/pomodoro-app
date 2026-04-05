97f32fcd604ac58f1e973b3ee2a9b70aab1b42a4
malika19-cmd <wadhwamalika50@gmail.com>
2026-04-05 10:59:39 +0530

    server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express"); const http = require("http"); const { Server } = require("socket.io");
 const app = express(); const server = http.createServer(app); const io = new Server(server);
-app.use(express.static("public"));
+app.use(express.static("."));
 
 let settings = {
   pomodoro: 25,
